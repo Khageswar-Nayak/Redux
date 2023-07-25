fd2f221b447709a5d2de91aaa5cae0e478ff53d9
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-07-25 15:42:45 +0530

    increasing and decreasing the value by two

--- redux-demo.js
+++ redux-demo.js
@@ -1,11 +1,13 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  } else {
-    return { counter: state.counter + 1 };
+  if (action.type === "INCREMENTBY2") {
+    return { counter: state.counter + 2 };
   }
+  if (action.type === "DECREMENTBY2") {
+    return { counter: state.counter - 2 };
+  }
+  return state;
 };
 const store = redux.createStore(counterReducer);
 
@@ -18,6 +20,5 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 
-for (let i = 0; i < 4; i++) {
-  store.dispatch({ type: "increment" });
-}
+store.dispatch({ type: "INCREMENTBY2" });
+store.dispatch({ type: "DECREMENTBY2" });
